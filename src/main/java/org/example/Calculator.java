baa628195e1197bd5d4d4bea1b87a6c197b7fe0d
Ankit Gupta <ankitgupta122003@gmail.com>
2025-02-10 12:46:22 +0530

    Completed the main class

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -1,7 +1,21 @@
 package org.example;
 
-public class Calculator {
-    public static void main(String[] args) {
+import java.util.List;
+import java.util.Scanner;
 
+public class Calculator {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the expression: ");
+        String str = sc.nextLine().replaceAll("\\s+" , "");
+
+        try {
+            double res = evaluteExpression(str);
+            System.out.println("Output: " + res);
+        } catch (Exception e) {
+            System.out.println("Error occurred: " + e.getMessage());
+        }
     }
 }
